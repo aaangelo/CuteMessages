5202b3f12dc7683d454d293c1f7b270ead739485
aaangelo <100152932+aaangelo@users.noreply.github.com>
2022-04-18 21:48:08 +0100

    added script
    A few arrays for sandwich fillings, will fill out a few options for each and then add logic next to generate from a  few random numbers.

---
+++ script.js
@@ -0,0 +1,15 @@
+// Sandwich components
+//bread
+const bread = ['White Bloomer', 'Seedy Brown', 'Tiger', 'Baguette', 'Sour Dough']
+
+//Main filling
+const main = ['Chicken', 'Beef', 'Ham', 'Tuna']
+
+//cheese?
+const cheese = []
+
+//salad
+const salad = []
+
+//sauce
+const sauce = []
