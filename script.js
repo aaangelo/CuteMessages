22bac840f37d27d48f3720d79f292d82bb20a7b2
aaangelo <100152932+aaangelo@users.noreply.github.com>
2022-04-19 19:46:10 +0100

    sandwich Gen
    added a bunch more parameters and created logic for sandwich gen - started creating logic for random salad amount, not implemented yet

--- script.js
+++ script.js
@@ -1,15 +1,31 @@
 // Sandwich components
 //bread
-const bread = ['White Bloomer', 'Seedy Brown', 'Tiger', 'Baguette', 'Sour Dough']
+const bread = ['white Bloomer', 'seedy Brown', 'tiger', ' french baguette', 'sour dough']
 
 //Main filling
-const main = ['Chicken', 'Beef', 'Ham', 'Tuna']
-
-//cheese?
-const cheese = []
+const main = ['chicken', 'beef', 'ham', 'tuna', 'cheese', 'meatballs', 'falafel', 'bacon', 'crisps']
 
 //salad
-const salad = []
+const salad = ['tomato', 'lettuce', 'onion', 'cucumber','pepper', 'sweetcorn', 'jalapenos', 'olives']
 
-//sauce
-const sauce = []
+//Sandwich Gen
+function makeSandwich() {
+    let breadChoice = bread[Math.floor(Math.random() * 5)];
+    let mainChoice = main[Math.floor(Math.random() * 8)];
+    //let saladAmount = Math.floor(Math.random() * (8 - 2 + 1) + 2)
+    let saladChoice = salad[Math.floor(Math.random() * 8)];
+    let withCheese = Math.floor(Math.random * 1);
+    let firstPart = `Your sandwich will be ${mainChoice}`;
+    let secondPart = '';
+    if (mainChoice != 'cheese' || mainChoice != 'crisps' && withCheese === 1) {
+        secondPart = ` with cheese and ${saladChoice}, on ${breadChoice} bread.`;
+    } else if (mainChoice === 'crisps') {
+        secondPart = ` on ${breadChoice}.`;
+    } else {
+        secondPart = ` and ${saladChoice}, on ${breadChoice} bread.`;
+    }
+    return firstPart + secondPart;
+
+}
+
+console.log(makeSandwich())
